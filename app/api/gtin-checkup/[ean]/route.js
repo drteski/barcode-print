7e8fcb35c6bbbaed754b13b58505ad6dde04dec8
Krzysztof Tomaszewski <drteski@gmail.com>
2024-07-18 08:59:29 +0200

    init

--- app/api/gtin-checkup/[ean]/route.js
+++ app/api/gtin-checkup/[ean]/route.js
@@ -1,20 +1,18 @@
 import { NextResponse } from 'next/server';
 import puppeteer from 'puppeteer';
-import playwright from 'playwright'
+import playwrite from 'playwright'
 
 export const dynamic = 'force-dynamic';
 
 export async function GET(request, { params }) {
 	const { ean } = params;
+	console.log(ean);
 	const getGtinData = async () => {
-
-		const browser = await playwright.chromium.launch({
-			headless: true // set this to true
+		const browser = await puppeteer.launch({
+			headless: true,
 		});
-
 		const page = await browser.newPage();
 		await page.goto(`https://www.eprodukty.gs1.pl/catalog/${ean}`);
-
 
 		const scrapedData = await page.waitForSelector('.main__header', { timeout: 3000 }).then(res => res).catch(error => '');
 
@@ -26,6 +24,7 @@ export async function GET(request, { params }) {
 
 		}
 	};
+
 	const data = await getGtinData();
 
 	return NextResponse.json({ data });
