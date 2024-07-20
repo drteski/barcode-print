746dfb4d64b0a75faa1316f9daaab0b80e054160
Krzysztof Tomaszewski <drteski@gmail.com>
2024-07-20 18:10:19 +0200

    init

--- app/api/gtin-checkup/[ean]/route.js
+++ app/api/gtin-checkup/[ean]/route.js
@@ -3,7 +3,7 @@ import puppeteer from 'puppeteer';
 import chromium from '@sparticuz/chromium';
 
 export const dynamic = 'force-dynamic';
-export const maxDuration = 300;
+export const maxDuration = 60;
 
 export async function GET(request, { params }) {
 	const { ean } = params;
